7fb94d17eac8706be0254000fe457e4c5e94e69a
Álvaro Medeiros <alvaro.medeiros@hp.com>
2023-11-07 21:31:07 -0300

    Ajustes

--- src/Components/header/index.tsx
+++ src/Components/header/index.tsx
@@ -1,21 +1,13 @@
 import Logo from '@assets/Logo.svg';
 import { HeaderContainer, InfosContainer } from './styled';
-import { Location } from '@components/location';
 import { MapPinLine } from '@phosphor-icons/react';
-import { Cart } from '@components/cart';
 
 export function Header() {
     return (
         <HeaderContainer>
             <img src={Logo} />
             <InfosContainer>
-                <Location
-                    location="Choro, CE"
-                    icon={<MapPinLine weight="fill" />}
-                    $background="brand-purple-light"
-                    $color="brand-purple-dark"
-                />
-                <Cart />
+                <MapPinLine />
             </InfosContainer>
         </HeaderContainer>
     );
