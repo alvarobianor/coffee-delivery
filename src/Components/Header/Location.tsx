36cc29111b6af41608e97557205ba1530a3d8d4c
Álvaro Medeiros <alvaro.medeiros@hp.com>
2023-11-11 19:55:19 -0300

    Creating the Location component

---
+++ src/Components/Header/Location.tsx
@@ -0,0 +1,31 @@
+import { MapPinLine, Icon } from '@phosphor-icons/react';
+import styled from 'styled-components';
+
+type Props = {
+  text: string;
+  Icon?: Icon;
+};
+
+export function Location({ text, Icon = MapPinLine }: Props) {
+  return (
+    <ContainerLocation>
+      <Icon size={22} />
+      {text}
+    </ContainerLocation>
+  );
+}
+
+const ContainerLocation = styled.button`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+
+  padding: 0.625rem 0.5rem;
+
+  background-color: ${({ theme }) => theme.colors['brand-purple-light']};
+  color: ${({ theme }) => theme.colors['brand-purple-dark']};
+  font-weight: 500;
+
+  border: 0px;
+  border-radius: 6px;
+`;
