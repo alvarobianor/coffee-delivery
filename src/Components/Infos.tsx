0f0c4fc189d43ff2b7b066b1f9934f9fa36668b2
Álvaro Medeiros <alvaro.medeiros@hp.com>
2023-11-12 14:24:53 -0300

    Updating info component

--- src/Components/Infos.tsx
+++ src/Components/Infos.tsx
@@ -13,8 +13,9 @@ const Container = styled.div`
   width: 100%;
   height: 34rem;
 
-  background-color: ${({ theme }) => theme.colors['base-error']};
+  /* background-color: ${({ theme }) => theme.colors['base-error']}; */
 
+  border: 1px solid black;
   display: flex;
   align-items: center;
   justify-content: center;
