84b3efc7ba3161d6c346171d0242aa794b33259a
Álvaro Bianor <bianor.alvaro12@gmail.com>
2023-11-12 21:12:14 -0300

    Update GlobalStyles.ts

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -1,7 +1,7 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
-
+/* TODO use media queries or similar */
   *{
     margin: 0 ;
     padding: 0;
