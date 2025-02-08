cc35a21998129c714e3b09c7a9b0aafb9eaec8b5
Álvaro Bianor <bianor.alvaro12@gmail.com>
2025-02-08 18:48:19 -0300

    move some styles from global styles

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -1,4 +1,4 @@
-import { createGlobalStyle } from 'styled-components';
+import styled, { createGlobalStyle, DefaultTheme } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
 /* TODO use media queries or similar */
@@ -29,4 +29,38 @@ export const GlobalStyle = createGlobalStyle`
     text-decoration: none;
   }
 
+`;
+export const ContainerMain = styled.main`
+  display: flex;
+  gap: 2rem;
+  width: 100%;
+  flex-direction: column;
+
+  background-color: ${({ theme }) => theme.colors['base-card']};
+  padding: 2.5rem;
+  border-radius: 6px;
+`;
+
+export const ContainerHeader = styled.header`
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  justify-content: flex-start;
+  gap: 0.5rem;
+`;
+
+interface IconProps {
+  $color: keyof DefaultTheme['colors'];
+}
+
+export const Icon = styled.div<IconProps>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: ${({ theme, $color }) => theme.colors[$color]};
+`;
+
+export const ContainerTitle = styled.div`
+  display: flex;
+  flex-direction: column;
 `;
