df896b4b7037fb8a77d60b5671006405b9ebf0a1
Álvaro Bianor <bianor.alvaro12@gmail.com>
2025-02-08 12:53:25 -0300

    Start doing the header of component

--- src/Components/FinishOrder/index.tsx
+++ src/Components/FinishOrder/index.tsx
@@ -1,9 +1,16 @@
+import { MapPinLine } from '@phosphor-icons/react';
 import { styled } from 'styled-components';
 
 export function FinishOrder() {
   return (
     <Container>
-      <h1> Finish Order</h1>
+      <ContainerHeader>
+        <MapPinLine size={22} />
+        <ContainerTitle>
+          <h3>Endereço de Entrega</h3>
+          <p>Informe o endereço onde deseja receber seu pedido</p>
+        </ContainerTitle>
+      </ContainerHeader>
     </Container>
   );
 }
@@ -14,5 +21,23 @@ const Container = styled.main`
   flex-direction: column;
 
   background-color: ${({ theme }) => theme.colors['base-card']};
-  padding: 1rem;
+  padding: 2.5rem;
+  border-radius: 6px;
+
+  svg {
+    color: ${({ theme }) => theme.colors['brand-yellow-dark']};
+  }
+`;
+
+const ContainerHeader = styled.header`
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  justify-content: flex-start;
+  gap: 0.5rem;
+`;
+
+const ContainerTitle = styled.div`
+  display: flex;
+  flex-direction: column;
 `;
