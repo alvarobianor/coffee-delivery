3122f1cacac112f8fc140c16735c2977adce0ebe
Álvaro Bianor <bianor.alvaro12@gmail.com>
2025-02-07 21:27:29 -0300

    Start the styles of components

--- src/Components/FinishOrder/index.tsx
+++ src/Components/FinishOrder/index.tsx
@@ -1,3 +1,18 @@
+import { styled } from 'styled-components';
+
 export function FinishOrder() {
-  return <h1> Finish Order</h1>;
+  return (
+    <Container>
+      <h1> Finish Order</h1>
+    </Container>
+  );
 }
+
+const Container = styled.main`
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+
+  background-color: ${({ theme }) => theme.colors['base-card']};
+  padding: 1rem;
+`;
